184a36eeb8f75d9cb7ea02adb5531e7ea08dc341
Jordan Whitehead <jordan9001@gmail.com>
2019-01-01 20:52:26 -0500

    Added calendar html, have to do the pluming now

--- site/js/mealplan.js
+++ site/js/mealplan.js
@@ -5,10 +5,22 @@ const recKey = "recipes";
 const calKey = "calendar";
 
 class FTCalendar {
+	//
+	// calobj = {recipe="", servings=0, date=0}
+	//
+
 	constructor(ftool) {
 		this.ftool = ftool;
 		// TODO remove outdated entries?
 		this.calarr = [];
+
+		// initialize the calendar numbers
+	}
+
+	recalcCosts(recname, updateall=false) {
+		//TODO
+		// thie recipe has updated
+		// find all uses of it and update costs
 	}
 
 	loadSaved() {
@@ -108,10 +120,76 @@ class FTRecipes {
 		}, false);
 	}
 
-	recalcCosts() {
+	recalcCosts(ingname, updateall=false) {
 		// gets called when ingredients are updated/added/removed
 		// so costs could have changed
-		//TODO
+		// any recipe effected should call recalcCosts in the calendar
+		
+		// loop through all our recipes, and find any that depend on the ing
+		let recnodes = document.querySelectorAll(".recipe_t_entry");
+		let that = this;
+		this.recarr.forEach(function(recobj) {
+			let changed = false;
+			let cost = 0.0;
+			// first find the html node
+			let node = null;
+			for (let i=0; i<recnodes.length; i++) {
+				if (recnodes[i].querySelector(".rec_t_title").textContent.trim() === recobj.name) {
+					node = recnodes[i];
+					break;
+				}
+			}
+			if (node === null) {
+				console.log("Bad! Recipe array and html are out of sync");
+				return;
+			}
+
+			recobj.ing.forEach(function(ingent) {
+				let price = 0;
+				let unit = "???";
+				let totcost = 0;
+				let ing = that.ftool.ingredients.get(ingent.name);
+				if (ing !== null) {
+					price = ing.price;
+					unit = ing.unit;
+					totcost = price * ingent.amount;
+					if (cost != null) {
+						cost += totcost;
+					}
+				} else {
+					// we have an unknown ingrediant, so the cost is unknown
+					cost = null;
+				}
+				// if this ingrediant is the one we are looking for, we have to update it's html node as well
+				if (updateall || ingname.toLowerCase() == ingent.name.toLowerCase()) {
+					changed = true;
+
+					// find it's html node
+					let ingnodes = node.querySelectorAll(".ingrediententry_t_entry");
+					for (let i=0; i<ingnodes.length; i++) {
+						let spans = ingnodes[i].querySelectorAll("span");
+						if (spans[0].textContent === ingent.name) {
+							// correct it
+							spans[1].textContent = ingent.amount;
+							spans[2].textContent = unit +"s";
+							spans[3].textContent = (ing !== null) ? "$"+price.toFixed(2) : "???";
+							spans[4].textContent = unit;
+							spans[5].textContent = (ing !== null) ? "$"+totcost.toFixed(2) : "???";
+						}
+					}
+				}
+			});
+
+			if (changed) {
+				// update the overall as well.
+				node.querySelector(".rec_t_cost").textContent = "$" + ((cost === null) ? "???" : cost.toFixed(2));
+				node.querySelector(".rec_t_perserv").textContent = (cost === null) ? "???" : cost / recobj.serves;
+
+				that.ftool.calendar.recalcCosts(recobj.name);
+			}
+		});
+		// update the recipe being added as well
+
 	}
 
 	addlists(recobj) {
@@ -134,7 +212,12 @@ class FTRecipes {
 				price = ing.price;
 				unit = ing.unit;
 				totcost = price * ingent.amount;
-				cost += totcost;
+				if (cost != null) {
+					cost += totcost;
+				}
+			} else {
+				// we have an unknown ingrediant, so the cost is unknown
+				cost = null;
 			}
 			let ingcln = document.importNode(ingtpt.content, true);
 			let spans = ingcln.querySelectorAll('span');
@@ -178,20 +261,27 @@ class FTRecipes {
 
 	add(recobj) {
 		// Do checks
-		let exists = false;
-		this.recarr.forEach(function (obj) {
-			if (obj.name === recobj.name) {
-				exists = true;
-			}
-		});
-		if (exists || recobj.name === "" || recobj.name == undefined) {
+		recobj.name = recobj.name.trim();
+		if (this.get(recobj.name) !== null) {
 			M.toast({html:"Recipe "+recobj.name+" already exisits", classes:"red"});
+			return;
+		}
+		if (recobj.name === "" || recobj.name == undefined) {
+			M.toast({html:"Recipe name required", classes:"red"});
+			return;
+		}
+		if (isNaN(recobj.serves)) {
+			M.toast({html:"Invalid Amount served for Recipe", classes:"red"});
 			return;
 		}
 
 		this.recarr.push(recobj);
 
 		this.addlists(recobj);
+
+		this.ftool.calendar.recalcCosts(recobj.name);
+
+		this.doSave();
 	}
 
 	rem(recname) {
@@ -208,9 +298,21 @@ class FTRecipes {
 
 				// remove from list
 				this.recarr.splice(i, 1);
+				this.ftool.calendar.recalcCosts(recname);
+				this.doSave();
 				return;
 			}
 		}
+	}
+
+	get(recname) {
+		recname = recname.trim().toLowerCase();
+		for (let i=0; i<this.recarr.length; i++) {
+			if (this.recarr[i].name.toLowerCase() === recname) {
+				return this.recarr[i];
+			}
+		}
+		return null;
 	}
 
 	loadSaved() {
@@ -220,6 +322,11 @@ class FTRecipes {
 		}
 
 		this.recarr = JSON.parse(saved);
+		let that = this;
+		this.recarr.forEach(function(recobj) {
+			that.addlists(recobj);
+		});
+		//this.ftool.calendar.recalcCosts(recobj.name, true);
 	}
 
 	doSave() {
@@ -296,15 +403,22 @@ class FTingredients {
 	}
 
 	add(ingobj) {
+		ingobj.name = ingobj.name.trim();
 		// do checks
-		let exists = false;
-		this.ingarr.forEach(function (obj) {
-			if (obj.name === ingobj.name || ingobj.name == undefined) {
-				exists = true;
-			}
-		});
-		if (exists || ingobj.name === "") {
+		if (this.get(ingobj.name) !== null) {
 			M.toast({html:"Ingredient "+ingobj.name+" already exisits", classes:"red"});
+			return;
+		}
+		if (ingobj.name === "" || ingobj.name == undefined) {
+			M.toast({html:"Ingredient name required", classes:"red"});
+			return;
+		}
+		if (isNaN(ingobj.price)) {
+			M.toast({html:"Invalid price for Ingredient", classes:"red"});
+			return;
+		}
+		if (ingobj.unit.trim() === "") {
+			M.toast({html:"Invalid unit for Ingredient", classes:"red"});
 			return;
 		}
 
@@ -313,7 +427,8 @@ class FTingredients {
 		this.addlists(ingobj);
 
 		// pass on any needed info to the recipes
-		this.ftool.recipes.recalcCosts();
+		this.ftool.recipes.recalcCosts(ingobj.name);
+		this.doSave();
 	}
 
 	rem(ingname) {
@@ -341,17 +456,19 @@ class FTingredients {
 
 				// remove from list
 				this.ingarr.splice(i, 1);
+				this.doSave();
 				return;
 			}
 		}
 
 		// pass on any needed info to the recipes
-		this.ftool.recipes.recalcCosts();
+		this.ftool.recipes.recalcCosts(ingname);
 	}
 
 	get(ingname) {
+		ingname = ingname.trim().toLowerCase();
 		for (let i=0; i<this.ingarr.length; i++) {
-			if (this.ingarr[i].name === ingname) {
+			if (this.ingarr[i].name.toLowerCase() === ingname) {
 				return this.ingarr[i];
 			}
 		}
@@ -365,6 +482,11 @@ class FTingredients {
 		}
 
 		this.ingarr = JSON.parse(saved);
+
+		let that = this;
+		this.ingarr.forEach(function(ingobj) {
+			that.addlists(ingobj);
+		});
 	}
 
 	doSave() {
@@ -417,7 +539,3 @@ M.Collapsible.init(
 // adjust the containers
 document.querySelector('.carousel').style.height = "100vh";
 
-// debug add a few ingredients and recipes
-ft.ingredients.add({
-	name:"Bananas", price:0.25, unit:"Banana"
-});
